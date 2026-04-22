b7eba63285e5d9fe2b1301fbf6d07871163e69e7
Ivan <ivanitit3535@gmail.com>
2026-04-22 20:12:43 +0300

    feat: add header

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,11 +3,13 @@ import { RouterView } from 'vue-router';
 import './styles/style-tailwind.css';
 import ScrollUpButton from '@/shared/ui/ScrollUpButton';
 import Footer from '@/widgets/footer';
+import Header from '@/widgets/header';
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col overflow-clip">
     <div class="max-w-desktop mx-auto flex w-full flex-1 flex-col px-4">
+      <Header />
       <main class="flex-1">
         <RouterView v-slot="{ Component }">
           <transition name="fade" mode="out-in">
@@ -15,7 +17,6 @@ import Footer from '@/widgets/footer';
           </transition>
         </RouterView>
       </main>
-
       <Footer />
       <ScrollUpButton />
     </div>
